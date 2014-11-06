14eb294013ffb53f101b7d045bd8656f79c6f44b
sazamore <sharri@u.washington.edu>
2014-11-06 12:23:18 -0800

    it's a function now

--- simple_script.py
+++ simple_script.py
@@ -12,6 +12,18 @@ Created on Tue Nov 04 17:38:57 2014
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.random.normal(0,2,(75,1));
-plt.hist(x)
-plt.show()
+#make it into a function!
+def show_normhist(data_length=1000, bins=100):
+    """Create 1d data with normal distribution, then plot it as a histogram."""
+    
+    x = np.random.normal(0,2,data_length);
+    plt.hist(x,bins)
+    plt.show()
+
+#wanna call it from the command line?
+def main():
+    show_normhist(10000,50) #call the function and give the paramaters, if applicable
+
+#call function
+if __name__ == '__main__':
+    main()
